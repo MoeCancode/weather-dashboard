438f156fcb30789e4f6207dc19a0780eb02ee47d
M A Razvi <abuzarflw@gmail.com>
2022-04-10 23:54:06 -0700

    Add placeholders for today's weather measures. Add styling to them

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 var apiKey = "ec1adb15cd1e6cb24a504f57722bba8d"
+var searchButton = document.querySelector("#search");
 
-
-function fetchGeoCode(cityname, statecode, countrycode) {
-    var geoCodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname},${statecode},${countrycode}&appid=${apiKey}`
-   var geoData = fetch(geoCodeURL).then(response => response.json())
-    .then(data => data);
-    return geoData;
-}
-
-async function fetchWeather() {
-    var geoDataObject = await fetchGeoCode("Danville", "CA", "US");
+//
+async function fetchWeather(searchBarInput) {
+    var geoDataObject = await fetchGeoCode(searchBarInput);
     console.log(geoDataObject);
     var lon = geoDataObject[0].lon;
     var lat = geoDataObject[0].lat;
     var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
     
     fetch(weatherURL).then(response =>response.json()).then(data => console.log(data));
+    // displayData(data);
 }
-fetchWeather();
+
+
+function fetchGeoCode(cityname) {
+    var geoCodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&appid=${apiKey}`
+   var geoData = fetch(geoCodeURL).then(response => response.json())
+    .then(data => data);
+    return geoData;
+}
+
+searchButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    var chosenCity = document.querySelector("#searchForm").value;
+    fetchWeather(chosenCity);
+})
